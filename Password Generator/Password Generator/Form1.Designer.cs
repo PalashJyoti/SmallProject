6345440469978fa58435ce9bb1c3df763b5aecb2
Palash Jyoti Goswami <palashjyotigoswamipt@gmail.com>
2021-02-04 22:55:50 +0530

    ver0.0.1

--- Password Generator/Password Generator/Form1.Designer.cs
+++ Password Generator/Password Generator/Form1.Designer.cs
@@ -29,12 +29,16 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.strong = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -43,37 +47,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(241, 22);
             this.textBox1.TabIndex = 0;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(268, 211);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(111, 21);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "8 Characters";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(268, 238);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(119, 21);
-            this.checkBox2.TabIndex = 2;
-            this.checkBox2.Text = "10 Characters";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(268, 265);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(119, 21);
-            this.checkBox3.TabIndex = 3;
-            this.checkBox3.Text = "12 Characters";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
@@ -93,6 +67,7 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "Check";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -104,20 +79,98 @@
             this.label1.Text = "Enter your password to check security or Generate.";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(268, 223);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(110, 21);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "8 Characters";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(268, 250);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(118, 21);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "10 Characters";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(269, 277);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(118, 21);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "12 Characters";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "week!";
+            this.label2.Visible = false;
+            // 
+            // strong
+            // 
+            this.strong.AutoSize = true;
+            this.strong.Location = new System.Drawing.Point(266, 188);
+            this.strong.Name = "strong";
+            this.strong.Size = new System.Drawing.Size(48, 17);
+            this.strong.TabIndex = 11;
+            this.strong.Text = "strong";
+            this.strong.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(268, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "strong";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "very strong";
+            this.label4.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.strong);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Password Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,12 +179,16 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label strong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Password Generator/Password Generator/Form1.cs
+++ Password Generator/Password Generator/Form1.cs
@@ -12,6 +12,7 @@ namespace Password_Generator
 {
     public partial class Form1 : Form
     {
+        string password;
         public Form1()
         {
             InitializeComponent();
@@ -22,14 +23,9 @@ namespace Password_Generator
 
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
         public void Generate(int len)
         {
-            const string validChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789~`!@#$%^&*()_-+={[}]|\"\\:;'<,>.?/'";
+            const string validChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`!@#$%^&*()_-+={[}]|\"\\:;'<,>.?/'";
             StringBuilder result = new StringBuilder();
             Random rand = new Random();
             while (0 < len--)
@@ -39,29 +35,84 @@ namespace Password_Generator
             textBox1.Text = result.ToString();
         }
 
+        public void Check()
+        {
+            int l = 0, u = 0, n = 0, s = 0, t = 0;
+            const string lower = "abcdefghijklmnopqrstuvwxyz";
+            const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string number = "0123456789";
+            const string sign = "~`!@#$%^&*()_-+={[}]|\"\\:;'<,>.?/'";
+            for (int i=0;i<password.Length;i++)
+            {
+                char[] a = password.ToCharArray();
+                if(lower.Contains(a[i]))
+                {
+                    l = 1;
+                }
+                if (upper.Contains(a[i]))
+                {
+                    u = 1;
+                }
+                if (number.Contains(a[i]))
+                {
+                    n = 1;
+                }
+                if (sign.Contains(a[i]))
+                {
+                    s = 1;
+                }
+            }
+            t = l + u + s + n;
+            if (t==4)
+            {
+                label4.Visible = true;
+                label2.Visible = false;
+                label3.Visible = false;
+            }
+            else if(t==3)
+            {
+                label3.Visible = true;
+                label2.Visible = false;
+                label4.Visible = false;
+            }
+            else if(t==1||t==2)
+            {
+                label2.Visible = true;
+                label3.Visible = false;
+                label4.Visible = false;
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            int len = 6;
-            /*const string validChar="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789~`!@#$%^&*()_-+={[}]|\"\\:;'<,>.?/'";
-            StringBuilder result = new StringBuilder();
-            Random rand = new Random(); 
-            while(0<len--)
-            {
-                result.Append(validChar[rand.Next(validChar.Length)]);
-            }
-            textBox1.Text = result.ToString();*/
-            if(checkBox1.Checked)
+            
+            if(radioButton1.Checked)
             {
                 Generate(8);
             }
-            else if(checkBox2.Checked)
+            else if(radioButton2.Checked)
             {
                 Generate(10);
             }
-            else if(checkBox3.Checked)
+            else if(radioButton3.Checked)
             {
                 Generate(12);
             }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            password = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Check();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximizeBox = false;
         }
     }
 }
